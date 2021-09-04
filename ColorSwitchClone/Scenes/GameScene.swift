b5d79c07141c88e1ad1ecd60be24348dcf0e7ccf
Sam Reaves <thesamreaves@gmail.com>
2021-09-04 17:45:52 -0400

    Set up some basic physics

--- ColorSwitchClone/Scenes/GameScene.swift
+++ ColorSwitchClone/Scenes/GameScene.swift
@@ -12,23 +12,35 @@ class GameScene: SKScene {
     var colorSwitch: SKSpriteNode!
     
     override func didMove(to view: SKView) {
+        setupPhysics()
         layoutScene()
     }
-    
+
+    func setupPhysics() {
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -2.0)
+    }
+
     func layoutScene() {
         backgroundColor = UIColor(red: 44/255, green: 62/255, blue: 80/255, alpha: 1.0)
         colorSwitch = SKSpriteNode(imageNamed: "colorcircle")
         colorSwitch.size = CGSize(width: frame.size.width / 3, height: frame.size.width / 3)
         colorSwitch.position = CGPoint(x: frame.midX, y: frame.minY + colorSwitch.size.height)
+        colorSwitch.physicsBody = SKPhysicsBody(circleOfRadius: colorSwitch.size.width / 2)
+        colorSwitch.physicsBody?.categoryBitMask = PhysicsCategories.switchCategory
+        colorSwitch.physicsBody?.isDynamic = false
         addChild(colorSwitch)
         
         spawnBall()
     }
-    
+
     func spawnBall() {
         let ball = SKSpriteNode(imageNamed: "ball")
-        ball.size = CGSize(width: 20, height: 20)
+        ball.size = CGSize(width: 30.0, height: 30.0)
         ball.position = CGPoint(x: frame.midX, y: frame.maxY - ball.size.height)
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2)
+        ball.physicsBody?.categoryBitMask = PhysicsCategories.ballCategory
+        ball.physicsBody?.contactTestBitMask = PhysicsCategories.switchCategory
+        ball.physicsBody?.collisionBitMask = PhysicsCategories.none
         addChild(ball)
     }
 }
---
+++ ColorSwitchClone/Template/Settings.swift
@@ -0,0 +1,14 @@
+//
+//  Settings.swift
+//  ColorSwitchClone
+//
+//  Created by Samuel Reaves on 9/4/21.
+//
+
+import SpriteKit
+
+enum PhysicsCategories {
+    static let none: UInt32 = 0
+    static let ballCategory: UInt32 = 0x1
+    static let switchCategory: UInt32 = 0x1 << 1
+}

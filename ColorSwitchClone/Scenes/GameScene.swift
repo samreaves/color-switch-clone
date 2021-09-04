6e7e38e12af6a9b00eb2a3c84cf2bcf4c3d59e5c
Sam Reaves <thesamreaves@gmail.com>
2021-09-04 17:15:26 -0400

    Deleted boilerplate code. Added makeshift assets while I wait on the Udemy course price to drop from 110 to 12 later.

---
+++ ColorSwitchClone/Scenes/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  ColorSwitchClone
+//
+//  Created by Samuel Reaves on 9/4/21.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
---
+++ ColorSwitchClone/ViewControllers/GameViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GameViewController.swift
+//  ColorSwitchClone
+//
+//  Created by Samuel Reaves on 9/4/21.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            let scene = SKScene(size: view.bounds.size)
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+            
+            // Present the scene
+            view.presentScene(scene)
+            
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
